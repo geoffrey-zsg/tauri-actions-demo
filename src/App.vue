00f79071e94f1f46a5a61f5c8bc0f4efe0b70282
geoffrey-zsg <1039597724@qq.com>
2024-01-23 15:53:39 +0800

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,43 @@
 <template>
   <div id="container">
+    <div id="menu">
+      <menu-left />
+    </div>
     <div id="main-content">
       <main id="content">
-        <h1>这是Tauri Demo 展示很多元素：</h1>
+        <h1>这是Electron Demo 简单展示很多元素：更新01-23 </h1>
         <a-grid :cols="24" :col-gap="16" :row-gap="16">
           <a-grid-item :span="24">
             <main-content />
           </a-grid-item>
         </a-grid>
       </main>
+      <div id="terminal">
+        <div id="title">终端展示：</div>
+        <a-textarea placeholder="请输入" allow-clear />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import NavigationMenu from './components/NavigationMenu.vue';
-import FolderTree from './components/FolderTree.vue';
 import MainContent from './components/MainContent.vue';
+import MenuLeft from './components/MenuLeft.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    NavigationMenu,
-    FolderTree,
+    MenuLeft,
     MainContent,
   },
   data() {
-    return {};
+    return {
+      // host: 'your.vnc.host.address',
+      // port: '5900',
+      // password: 'your-vnc-password',
+      // path: 'websockify', // 根据你的 VNC proxy/server 设置来配置
+    };
   },
 });
 </script>
@@ -36,12 +46,41 @@ export default defineComponent({
 <style scoped>
 #container {
   display: flex;
-  flex-direction: column;
   height: 100vh;
+}
+
+#menu {
+  flex: 0 0 300px;
 }
 
 #main-content {
   display: flex;
+  flex-direction: column;
   flex: 1;
+}
+
+aside {
+  width: 200px;
+  background-color: #f4f4f4;
+}
+
+nav {
+  width: 200px;
+  background-color: #ccc;
+}
+
+main {
+  padding: 20px;
+}
+
+#terminal {
+  margin-left: 20px;
+  width: 100%;
+  height: 200px;
+  border: 1px solid #ddd;
+  padding: 20px;
+}
+#title{
+  text-align: left;
 }
 </style>
---
+++ src/components/MenuLeft.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="menu-demo">
+    <a-menu :style="{ width: '200px', height: '100%' }" :default-open-keys="['0']" :default-selected-keys="['0_1']"
+      show-collapse-button>
+      <a-menu-item key="0_0_0" data-obj="1">文件夹 1</a-menu-item>
+      <a-sub-menu key="0">
+        <template #icon><icon-apps></icon-apps></template>
+        <template #title>文件夹 2</template>
+        <a-menu-item key="0_0">文件夹2-1</a-menu-item>
+        <a-menu-item key="0_1">文件夹2-2</a-menu-item>
+        <a-menu-item key="0_2" disabled>文件夹2-3</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="1">
+        <template #icon><icon-bug></icon-bug></template>
+        <template #title>文件夹3</template>
+        <a-menu-item key="1_0">文件夹3-1</a-menu-item>
+        <a-menu-item key="1_1">文件夹3-2</a-menu-item>
+        <a-menu-item key="1_2">文件夹3-3</a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="2">
+        <template #icon><icon-bulb></icon-bulb></template>
+        <template #title>文件夹4</template>
+        <a-menu-item-group title="Menu Group 1">
+          <a-menu-item key="2_0">文件夹4-1</a-menu-item>
+          <a-menu-item key="2_1">文件夹4-2</a-menu-item>
+        </a-menu-item-group>
+        <a-menu-item-group title="Menu Group 2">
+          <a-menu-item key="2_2">文件夹4-3</a-menu-item>
+          <a-menu-item key="2_3">文件夹4-4</a-menu-item>
+        </a-menu-item-group>
+      </a-sub-menu>
+    </a-menu>
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+import {
+  IconMenuFold,
+  IconMenuUnfold,
+  IconApps,
+  IconBug,
+  IconBulb,
+} from '@arco-design/web-vue/es/icon';
+
+export default defineComponent({
+  name: 'MenuLeft',
+  components: {
+    IconMenuFold,
+    IconMenuUnfold,
+    IconApps,
+    IconBug,
+    IconBulb,
+  },
+},
+);
+</script>
+<style scoped>
+.menu-demo {
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  padding: 40px;
+  background-color: var(--color-neutral-2);
+}
+</style>
